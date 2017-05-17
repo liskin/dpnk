33d8e502535b484912deef07a76ef3fd31229167
Tomas Janousek <tomi@nomi.cz>
2017-05-17 11:11:13 +0200

    Support for Strava activity URLs

--- dpnk_upload.pl
+++ dpnk_upload.pl
@@ -19,7 +19,7 @@ sub dpnk_login {
 	my %params = @_;
 
 	for (qw(username password)) {
-		croak "$_ param missing" unless exists $params{$_};
+		croak "$_ param missing" unless defined $params{$_};
 	}
 
 	my $token_query =
@@ -41,13 +41,13 @@ sub dpnk_upload_gpx {
 	my ($tok, %params) = @_;
 
 	for (qw(direction trip_date file)) {
-		croak "$_ param missing" unless exists $params{$_};
+		croak "$_ param missing" unless defined $params{$_};
 	}
 
 	my $gpx_content =
 		[ direction => $params{direction}
 		, trip_date => $params{trip_date}
-		, file => [ $params{file} ]
+		, file => $params{file}
 		];
 	my $r = $ua->post('https://dpnk.dopracenakole.cz/rest/gpx/',
 		Authorization => "Bearer $tok->{access_token}",
@@ -57,8 +57,19 @@ sub dpnk_upload_gpx {
 	undef;
 }
 
+sub strava_retrieve_gpx {
+	my ($url) = @_;
+
+	my $strava = Net::Netrc->lookup('www.strava.com');
+	my $strava_cookie = $strava->account // die 'pls put strava _strava3_session into ".netrc" as "account"';
+	my $r = $ua->get("$url/export_gpx", Cookie => $strava_cookie);
+	croak $r->status_line unless $r->is_success;
+
+	$r->decoded_content;
+}
+
 sub usage {
-	say "Usage: dpnk_upload.pl <date> <from|to> <file.gpx>";
+	say "Usage: dpnk_upload.pl <date> <from|to> <file.gpx|https://www.strava.com/activities/NNNNN>";
 	exit 0;
 }
 
@@ -68,7 +79,16 @@ my $file = shift // usage;
 
 die 'date must be YYYY-MM-DD' unless $date =~ /^\d\d\d\d-\d\d-\d\d$/;
 die 'direction must be from|to' unless $direction =~ /^(from|to)$/;
-die 'file does not exist' unless -e $file;
+
+if (-e $file) {
+	$file = [ $file ];
+} elsif ($file =~ m|https://www\.strava\.com/activities/(\d+)|) {
+	my $filename = $1 . ".gpx";
+	$file = [ undef, $filename, Content_Type => 'application/gpx+xml',
+		Content => strava_retrieve_gpx($file) ];
+} else {
+	die 'file does not exist or invalid URI supplied';
+}
 
 my $tok = dpnk_login_netrc();
 dpnk_upload_gpx($tok, direction => 'trip_' . $direction, trip_date => $date, file => $file);
